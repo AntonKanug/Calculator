ee60d800b7894c34454c86c62144248f071ae96a
Anton Kanugalawattage <antondilon@gmail.com>
2019-05-10 12:56:01 -0400

    Update Differential-Equations.py

--- Differential-Equations.py
+++ Differential-Equations.py
@@ -1,11 +1,11 @@
 '''
 Anton Kanugalwattage
 May 9, 2019
-Finding solution using Euler's Method for Differential Equations
+Finding solutions using Euler's Method for Differential Equations
 '''
 
 import pylab
-import math
+from math import *
     
 #Getting the function and information from user 
 funcInsert = input("Enter the function for dy/dx = f(x,y): (with correct math operations Eg:(2*x*y + x^2)) ")
@@ -31,16 +31,15 @@ def f(funcInsert,x,y):
 #Euler's Method
 x,y=[x0],[y0]
 for i in range(N):
-    x.append(x0+h*i)
+    x.append(x0+h*(i+1))
     y.append(y[i] + h*f(funcInsert,x[i],y[i]))
-    
 print("__________________________________________________________________________________________________")
 print("Solution using Euler's Method is: ")
 
 #Graphing the solution from Euler's Method
-pylab.plot(x,y)
+pylab.plot(x,y,)
 pylab.title("Solution for dy/dx = " + funcInsert)
 pylab.xlabel('x')
 pylab.ylabel('y')
+pylab.grid()
 pylab.show()
-
